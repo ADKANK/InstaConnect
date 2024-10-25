96b4f8bd04febc7bbb2bcea60e13fd3a1b1cc3ce
ADKANK <kumarana998@gmail.com>
2024-10-25 15:05:40 +0530

    Profile Image will be stored in Firebase storage during user registration

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ import dotenv from 'dotenv';
 import multer from 'multer';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import admin from 'firebase-admin';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
